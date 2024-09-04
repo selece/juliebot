637ae2a7b437f4a6a21547854f7a0d24cc27ad79
Julie Liaw <julie.k.liaw@gmail.com>
2024-09-04 00:54:14 -0600

    simplified db backing to sqlite3. added pun module.

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import os
+import requests
 import logging
+
+from datetime import datetime, timedelta
+
+from db import DB
 from twitchio.ext import commands, routines
 
 logger = logging.getLogger(__name__)
+db = DB.instance()
 
 class JulieBot(commands.Bot):
     def __init__(self):
@@ -14,14 +20,24 @@ class JulieBot(commands.Bot):
         )
 
         # routines 
-        for _r in [ self.automated_songbot_helper ]:
+        for _r in [ self.automated_songbot_helper, self.automated_chat_monitor, self.automated_pun_vote ]:
             _r.start()
 
         # vars
         self._last_message_was_me = False
+        self._last_active_chat_timestamp = datetime.now()
+
+        self._pun_vote_active = False
+        self._pun_vote_started_timestamp = datetime.now()
+        self._pun_votes = {}
+        self._pun_id = 0
+
+        # config
+        self.CHAT_INACTIVITY_TIMER = timedelta(minutes=1)
+        self.PUN_VOTE_WINDOW = timedelta(seconds=30)
 
     # message sender
-    async def send_message_to_chat(self, message):
+    async def send_message_to_chat(self, message: str):
         for channel in self.connected_channels:
             await channel.send(message)
 
@@ -30,21 +46,83 @@ class JulieBot(commands.Bot):
         logger.info(f'logged in as {self.nick} and user id {self.user_id}')
 
     # message handler
-    async def event_message(self, message):
+    async def event_message(self, message: str):
         if message.echo:
             self._last_message_was_me = True
             return
         
         else:
             self._last_message_was_me = False
+            self._last_active_chat_timestamp = datetime.now()
 
         await self.handle_commands(message)
 
     # songlist
     @commands.cooldown(rate=1, per=10, bucket=commands.Bucket.channel)
-    @commands.command()
+    @commands.command(aliases=("list", "sl", "slist"))
     async def songlist(self, ctx: commands.Context):
         await self.send_message_to_chat(f'hey @{ctx.author.name}, here\'s the song list link! https://www.streamersonglist.com/t/julie_never_streams/songs')
+
+    # random pun
+    @commands.cooldown(rate=1, per=30, bucket=commands.Bucket.channel)
+    @commands.command()
+    async def pun(self, ctx: commands.Context):
+        req: requests.Response = requests.get('https://punapi.rest/api/pun')
+
+        if req.status_code != 200:
+            await self.send_message_to_chat(f'sorry @{ctx.author.name}! the pun api does\'nt seem to be working right now... BibleThump')
+            return
+        
+        json_response = req.json()
+        pun_id: int = int(json_response['id'])
+        pun_text: str = json_response['pun']
+
+        logger.info(f'fetched pun: {pun_id} - {pun_text}')
+
+        pun_record: tuple = None
+        if db.check_if_pun_exists(pun_id):
+            pun_record = db.get_pun_from_db(pun_id)
+
+        else:
+            db.add_pun_to_db(pun_id)
+
+        self._pun_vote_active = True
+        self._pun_vote_started_timestamp = datetime.now()
+        self._pun_id = pun_id
+        await self.send_message_to_chat(f'i have a pun for you @{ctx.author.name}: {pun_text}')
+        await self.send_message_to_chat(f'please rate my pun using !ratepun <1-10>, 1 (terrible) to 10 (amazing)! voting will be open for the next 30 seconds... <3')
+
+        if pun_record is not None:
+            await self.send_message_to_chat(f'the last time this pun got picked, it was rated {pun_record[2]}')
+
+    # rate pun
+    @commands.cooldown(rate=1, per=30, bucket=commands.Bucket.user)
+    @commands.command(aliases=("rp"))
+    async def ratepun(self, ctx: commands.Context, rating: int):
+        if ctx.author.name in self._pun_votes:
+            return
+        
+        if not isinstance(rating, int):
+            return
+        
+        if not self._pun_vote_active:
+            return
+        
+        if rating > 10:
+            rating = 10
+
+        if rating < 1:
+            rating = 1
+        
+        self._pun_votes[ctx.author.name] = rating
+
+    # ban pun
+    @commands.command()
+    async def banpun(self, ctx: commands.Context):
+        if not ctx.author.is_broadcaster or ctx.author.is_mod:
+            return
+        
+        pass
 
     # routine definitions
     @routines.routine(minutes=5, wait_first=True)
@@ -54,6 +132,37 @@ class JulieBot(commands.Bot):
 
         else:
             logger.info(f'skipping automated songbot helper; last message was sent by me: {self._last_message_was_me}')
+
+    @routines.routine(minutes=2, wait_first=True)
+    async def automated_chat_monitor(self):
+        now_timestamp = datetime.now()
+        if now_timestamp - self._last_active_chat_timestamp > self.CHAT_INACTIVITY_TIMER:
+            pass
+
+    @routines.routine(seconds=1)
+    async def automated_pun_vote(self):
+        now_timestamp = datetime.now()
+        if self._pun_vote_active and now_timestamp - self._pun_vote_started_timestamp > self.PUN_VOTE_WINDOW:
+            self._pun_vote_active = False
+
+            average_rating = 0.0
+            total_votes = 0
+
+            if len(self._pun_votes) > 0:
+                for rating in self._pun_votes.values():
+                    average_rating += rating
+                    total_votes += 1
+
+                average_rating = average_rating / total_votes
+                await self.send_message_to_chat(f'closing pun voting! -- this pun will be rated {average_rating}')
+            else:
+                await self.send_message_to_chat(f'nobody liked that one... :( -- this pun will be rated {average_rating}')
+
+            
+            db.update_pun_rating(self._pun_id, average_rating)
+
+            self._pun_votes = {}
+            self._pun_id = 0
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)-15s [%(levelname)s] %(funcName)s: %(message)s")
--- db/__init__.py
+++ db/__init__.py
@@ -1,9 +1,7 @@
 import logging
-from datetime import datetime
-from sqlalchemy import create_engine, select, select_from, update, insert, DateTime
-from sqlalchemy.orm import sessionmaker
+import sqlite3
 
-from .models import Base, Viewer
+from datetime import datetime
 
 # constants
 DATABASE_PATH = "juliebot.db"
@@ -13,27 +11,85 @@ logger = logging.getLogger(__name__)
 class DB:
     __instance = None
 
-    def __new__(cls):
-        if cls.__instance is None:
-            cls.__instance = super(DB, cls).__new__(cls)
-            cls.__instance.__initialized = False
-        return cls.__instance
+    def __init__(self):
+        if DB.__instance is not None:
+            raise RuntimeError('cannot re-instantiate DB class')
+        else:
+            DB.__instance = self
+            self.conn = None
+            try:
+                self.conn = sqlite3.connect(DATABASE_PATH)
+                self.init_puns_db()
+            
+            except sqlite3.Error as e:
+                logging.error(f'failed to instantiate db: {e} - bot functionality will be limited')
+                return None
+            
+    
+    @staticmethod
+    def instance():
+        if DB.__instance is None:
+            DB()
 
-    def __self__(self):
-        if self.__initialized:
-            return
+        return DB.__instance
+    
+    def cleanup(self):
+        self.conn.close()
+
+    def run_sql_commit(self, sql_statement, vals=None):
+        try:
+            cursor = self.conn.cursor()
+            
+            if vals is None:
+                cursor.execute(sql_statement)
+            else:
+                cursor.execute(sql_statement, vals)
+
+            self.conn.commit()
+            logger.info(f'sql OK: {sql_statement} {vals}')
+            return cursor.lastrowid
+            
+        except sqlite3.Error as e:
+            logger.error(f'failed to execute sql statement \'{sql_statement}\' with vals {vals}: {e}')
+            return -1
+
+    
+    def init_puns_db(self):
+        sql = '''CREATE TABLE IF NOT EXISTS puns (id INTEGER PRIMARY KEY, api_id INTEGER NOT NULL UNIQUE, rating REAL NOT NULL, ban INT NOT NULL)'''
+        self.run_sql_commit(sql)
+
+    def add_pun_to_db(self, api_id):
+        sql = '''INSERT INTO puns(api_id, rating, ban) VALUES(?, ?, ?)'''
+        return self.run_sql_commit(sql, (api_id, 0, 0))
+    
+    def get_pun_from_db(self, api_id: int):
+        sql = '''SELECT * FROM puns WHERE api_id=?'''
+        cursor = self.conn.cursor()
+        cursor.execute(sql, (api_id))
+        rows = cursor.fetchall()
+
+        if len(rows) > 1:
+            logger.error(f'duplicate entry in db for api_id: {api_id}')
+            return None
         
-        self.__initialized = True
-        self.engine = create_engine(f'sqlite://{DATABASE_PATH}')
-        self.session = sessionmaker(self.engine, autoflush=True)
-        Base.metadata.create_all(self.engine)
+        if len(rows) == 0:
+            logger.info(f'no pun with api_id {api_id} found in db')
+            return None
+        
+        return rows[0]
 
-    def get_viewer(self, id):
-        with self.session() as sess:
-            query = (
-                select_from(Viewer)
-                    .where(Viewer.id == id)
-            )
-            result = sess.execute(query).one()
+    def update_pun_rating(self, api_id: int, rating: float):
+        sql = '''UPDATE puns SET rating=? WHERE api_id=?'''
+        return self.run_sql_commit(sql, (api_id, rating))
+    
+    def check_if_pun_exists(self, api_id: int):
+        sql = '''SELECT * FROM puns WHERE api_id=?'''
+        cursor = self.conn.cursor()
+        cursor.execute(sql, (api_id, ))
+        rows = cursor.fetchall()
 
-        return result
+        if len(rows) > 1:
+            logger.error(f'duplicate entry in db for api_id: {api_id}')
+            return True
+        
+        return len(rows) == 1

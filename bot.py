b530a57e4c3f667ee59344443bdcaf83f7ac1172
Julie Liaw <julie.k.liaw@gmail.com>
2025-03-14 01:36:35 -0600

    updated to twitchio3.0.0.b3, gutted most of the unused functionality. added raid message and adbreak functions

--- bot.py
+++ bot.py
@@ -1,49 +1,106 @@
 import os
 import logging
+import asyncio
+import sqlite3
+import asqlite
 
-from db import DB
-from cogs.puns import PunsCog
-from cogs.songs import SongsCog
-from cogs.mood import MoodCog
-from cogs.checkin import CheckInCog
-
+import twitchio
 from twitchio.ext import commands
+from twitchio import eventsub
 
 logger = logging.getLogger(__name__)
-db = DB.instance()
 
 class JulieBot(commands.Bot):
-    def __init__(self):
+    def __init__(self, *, token_database: asqlite.Pool) -> None:
+        self.token_database = token_database
         super().__init__(
-            token=os.environ['TOKEN'],
-            nick=os.environ['BOT_NICK'],
-            prefix=os.environ['BOT_PREFIX'],
-            initial_channels=[os.environ['CHANNEL']],
+            client_id=os.environ["CLIENT_ID"],
+            client_secret=os.environ["CLIENT_SECRET"],
+            bot_id=os.environ["BOT_ID"],
+            owner_id=os.environ["OWNER_ID"],
+            prefix="!",
         )
 
-        # cogs
-        self.add_cog(PunsCog(self))
-        self.add_cog(SongsCog(self))
-        self.add_cog(MoodCog(self))
-        self.add_cog(CheckInCog(self))
+    async def setup_hook(self) -> None:
+        await self.add_component(CustomCommandsComponent(self))
 
-    # message sender
-    async def send_message_to_chat(self, message: str):
-        for channel in self.connected_channels:
-            await channel.send(message)
+        subscription = eventsub.ChatMessageSubscription(broadcaster_user_id=os.environ["OWNER_ID"], user_id=os.environ["BOT_ID"])
+        await self.subscribe_websocket(payload=subscription)
 
-    # on connect
-    async def event_ready(self):
-        logger.info(f'logged in as {self.nick} and user id {self.user_id}')
+        subscription = eventsub.StreamOnlineSubscription(broadcaster_user_id=os.environ["OWNER_ID"])
+        await self.subscribe_websocket(payload=subscription)
 
-    # message handler
-    async def event_message(self, message: str):
-        if message.echo:
-            return
+    async def add_token(self, token: str, refresh: str) -> twitchio.authentication.ValidateTokenPayload:
+        resp: twitchio.authentication.ValidateTokenPayload = await super().add_token(token, refresh)
 
-        await self.handle_commands(message)
+        query = """
+        INSERT INTO tokens (user_id, token, refresh)
+        VALUES (?, ? ,?)
+        ON CONFLICT(user_id)
+        DO UPDATE SET
+            token = excluded.token,
+            refresh = excluded.refresh;
+        """
+
+        async with self.token_database.acquire() as connection:
+            await connection.execute(query, (resp.user_id, token, refresh))
+
+        logger.info("added token to db for user: %s", resp.user_id)
+        return resp
+    
+    async def load_tokens(self, path: str | None = None) -> None:
+        async with self.token_database.acquire() as connection:
+            rows: list[sqlite3.Row] = await connection.fetchall("""SELECT * FROM tokens""")
+
+        for row in rows:
+            await self.add_token(row["token"], row["refresh"])
+
+    async def setup_database(self) -> None:
+        query = """CREATE TABLE IF NOT EXISTS tokens(user_id TEXT PRIMARY KEY, token TEXT NOT NULL, refresh TEXT NOT NULL)"""
+        async with self.token_database.acquire() as connection:
+            await connection.execute(query)
+
+    async def event_ready(self) -> None:
+        logger.info("OK: logged in as %s", self.bot_id)
+
+
+class CustomCommandsComponent(commands.Component):
+    def __init__(self, bot: JulieBot):
+        self.bot = bot
+
+    @commands.Component.listener()
+    async def event_message(self, payload: twitchio.ChatMessage) -> None:
+        logger.info(f"[{payload.broadcaster.name}] - {payload.chatter.name}: {payload.text}")
+
+    @commands.command(aliases=["songs", "sl", "list"])
+    @commands.cooldown(rate=1, per=60, key=commands.BucketType.channel)
+    async def songlist(self, ctx: commands.Context) -> None:
+        await ctx.reply(f"@{ctx.chatter.name}, here's the link: https://t.ly/ezltR")
+
+    @commands.command()
+    @commands.is_elevated()
+    async def adbreak(self, ctx: commands.Context) -> None:
+        await ctx.send("!!! we're going on ad break shortly - we run 3m ads to fully poof away the pre-roll ads, so please sit tight! don't worry, no requests or music will be happening during the ads julien130Lopheart !!!")
+
+    @commands.command()
+    @commands.is_broadcaster()
+    async def raidmsg(self, ctx: commands.Context) -> None:
+        await ctx.send(f"!!! we're raiding out - please copy the following message and give them all of the warm and fuzzy plink-plonks when you arrive !!!")
+        await ctx.send("julien130Namalove plink plonks peeps have arrived! julien130Namalove")
+
+
+def main() -> None:
+    twitchio.utils.setup_logging(level=logging.INFO)
+
+    async def runner() -> None:
+        async with asqlite.create_pool("tokens.db") as token_database, JulieBot(token_database=token_database) as bot:
+            await bot.setup_database()
+            await bot.start()
+
+    try:
+        asyncio.run(runner())
+    except KeyboardInterrupt:
+        logger.warning("--- shutting down due to keyboard interrupt ---")
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(asctime)-15s [%(levelname)s] %(funcName)s: %(message)s")
-    bot = JulieBot()
-    bot.run()
+    main()
